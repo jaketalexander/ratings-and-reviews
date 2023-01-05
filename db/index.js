925802ddc34cd57f22f952239e7cc3a80c731e4b
Jake Alexander <jaketalexander@gmail.com>
2023-01-05 14:29:01 -0800

    DB Launched Adjustments

--- db/index.js
+++ db/index.js
@@ -2,10 +2,18 @@ const { Pool } = require('pg')
 require('dotenv').config();
 
 
+// const pool = new Pool({
+//     user: process.env.USER,
+//     database: process.env.DATABASE,
+//     port: process.env.PORT
+// })
+
 const pool = new Pool({
-    user: process.env.USER,
-    database: process.env.DATABASE,
-    port: process.env.PORT
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PORT,
 })
 
 pool.connect()
